6a95c400b29a88270bb70f8fc8930c062e338182
Wasiq Abdullah <samsunggrandprimeplus056@gmail.com>
2021-05-11 05:43:20 +0500

    Created task details route

--- base/views.py
+++ base/views.py
@@ -2,6 +2,7 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 # Create your views here.
 from .models import Task
 
@@ -9,3 +10,8 @@ from .models import Task
 class TaskList(ListView):
     model = Task
     context_object_name='tasks'
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name='task'
+    template_name='base/task.html'
