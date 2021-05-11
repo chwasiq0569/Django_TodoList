7a13ca310a64c88e8ac2ac2d2f074cd30d474f53
Wasiq Abdullah <samsunggrandprimeplus056@gmail.com>
2021-05-11 05:34:10 +0500

    rendered tasks

--- base/views.py
+++ base/views.py
@@ -1,9 +1,11 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.views.generic.list import ListView
 # Create your views here.
+from .models import Task
 
 
-def taskList(request):
-    return HttpResponse('Todo List App')
+class TaskList(ListView):
+    model = Task
+    context_object_name='tasks'
